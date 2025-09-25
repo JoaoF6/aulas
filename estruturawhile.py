e6749366013a03c8a052b47452c0522d10311689
JoaoF6 <jyouf231@gmail.com>
2025-09-25 21:27:04 +0000

    .

---
+++ estruturawhile.py
@@ -0,0 +1,14 @@
+''' 
+estrutura de repeticao com teste logico = while
+while not apple: (enquanto a condicao for verdadeira, o laco se repetira)
+    apple
+pega
+usar while quando nao souber o limite
+
+
+'''
+r = 's'
+while r == 's':
+    r = str(input('qual o valor? ' ))
+    
+ 
